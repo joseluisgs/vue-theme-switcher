ab57d1c63cb54d4f3b420ddb04d62a30af2f0ba7
joseluisgs <joseluisgonsan@gmail.com>
2020-12-12 18:17:19 +0100

    Temas en Vuex

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,51 @@
       <router-link to="/about">About</router-link>
     </div>
     <div class="selector">
-      <a href="#" @click="changeTheme('vue')" class="selector__link vue">Vue 🌞</a>
-      <a href="#" @click="changeTheme('night')" class="selector__link night">Night 🌚</a>
-      <a href="#" @click="changeTheme('eighties')" class="selector__link eighties">80's 🌈</a>
+      <a href="#" @click="cambiarTema('vue')" class="selector__link vue">Vue 🌞</a>
+      <a href="#" @click="cambiarTema('night')" class="selector__link night">Night 🌚</a>
+      <a href="#" @click="cambiarTema('eighties')" class="selector__link eighties">80's 🌈</a>
     </div>
     <router-view/>
   </div>
 </template>
 
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  name: 'App',
+  // Mi modelo de datos
+  data() {
+    return {
+      element: null,
+    };
+  },
+  // Al creame
+  created() {
+    // Cogemos el tema del DOM y lo almacenamos, es el elemento etiquetado como theme en index
+    this.element = document.getElementById('theme');
+    const theme = localStorage.getItem('theme');
+    // Según el tema lo cambiamos
+    if (theme) {
+      this.changeTheme(theme);
+    }
+  },
+  methods: {
+    ...mapActions(['changeTheme']),
+    // Cabiamos el tema, y disparamos la acción
+    // Para ello debemos recoger
+    cambiarTema(theme) {
+      // Asignamos el nuebo tema a este elemento
+      this.changeTheme({ theme, element: this.element });
+    },
+  },
+};
+</script>
+
 <style>
-html, body {
-  margin: 0;
-  padding: 0;
+  html, body {
+    margin: 0;
+    padding: 0;
 }
 </style>
 
--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,28 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    // Directorio donde se almacenan los temas
+    baseURL: process.env.BASE_URL,
+    // Nombre del tema principal
+    theme: 'vue',
   },
+  // Mutaciones, solo ellas tocan el estado
   mutations: {
+    changeTheme(state, theme) {
+      state.theme = theme;
+    },
   },
+  // Acciones
   actions: {
-  },
-  modules: {
+    // Acciones, sirven para cambiar varias cosas o trasformaciones asíncronas
+    changeTheme({ state, commit }, { theme, element }) {
+      // Cogemos y le cambiamos la parte de href
+      // eslint-disable-next-line no-param-reassign
+      element.href = `${state.baseURL}themes/${theme}/theme.css`;
+      // Lo almacenamos
+      localStorage.setItem('theme', theme);
+      // Ejecutamos el cambio
+      commit('changeTheme', theme);
+    },
   },
 });
